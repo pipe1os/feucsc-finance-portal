7e7877d6902e0db647b3a445c6b76d5c6eca2b27
pipe1os <farce@ing.ucsc.cl>
2025-04-24 07:58:26 -0400

    Modify vite.config.ts

--- feucsc/vite.config.ts
+++ feucsc/vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    visualizer({
-      // open: true,
-      // filename: "dist/stats.html",
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
